e45de3832c9dde21b9fd58ee2085b8055698f372
deborahcarvalho <deborah.carvalho@yuca.live>
2021-04-21 22:17:35 -0300

    Adding SSG

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,20 @@
-export default function Home() {
+export default function Home(props) {
   return (
     <div>
       <h1>Index</h1>
-      <p></p>
+      <p>{JSON.stringify(props.episodes)}</p>
     </div>
   )
+}
+
+export async function getStaticProps() {
+  const response = await fetch('http://localhost:3333/episodes');
+  const data = await response.json();
+
+  return {
+    props: {
+      episodes: data,
+    },
+    revalidate: 60 * 60 * 8, // recebe um numero em segundo de quanto em quanto tempo quero renovar a pagina
+  }
 }
